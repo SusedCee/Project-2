5198dd814aab78871885de8a128aa761f27f5191
Sandra Abernathy <sandraabernathy@Sandras-MacBook-Pro.local>
2019-07-26 11:14:55 -0600

    finished items views and items controller and misc

--- models/items.js
+++ models/items.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-const itemSchema = new Schema({
+const itemSchema = mongoose.Schema({
 	img: {type: String, required: true},
-	title: {type: String, required: true}
+	title: {type: String, required: true},
 	description: String,
 	user: {
 		type: mongoose.Schema.Types.ObjectId,
@@ -10,6 +10,6 @@ const itemSchema = new Schema({
 	}
 });
 
-const Item = mongoose.model("Item", photoSchema);
+const Item = mongoose.model("Item", itemSchema);
 
 module.exports = Item;
